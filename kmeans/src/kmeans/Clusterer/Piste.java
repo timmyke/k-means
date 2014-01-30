c672b41e26307636bce1e22dac6c84da94ee61ff
tim@server.com <>
2014-01-31 01:31:48 +0200

    Test

---
+++ kmeans/src/kmeans/Clusterer/Piste.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package kmeans.Clusterer;
+
+/**
+ *
+ * @author timi
+ */
+public class Piste {
+    // Nää on perkele propertyjä.
+    // Eli luvallista käyttää suoraan niitä perkeleen propertyjä
+    // ilman aksessorei. Eli ei vikistä siellä.
+    public double X;
+    public double Y;
+    public int Group;
+
+}
